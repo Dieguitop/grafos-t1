b3a6f0821f218877f59c3fb5df2de82fcb60286a
David Uribe <daviduribe998@gmail.com>
2021-04-18 02:34:14 -0400

    Matriz de adyacencia (más tests y Rscript)
    Correr en la raíz del repo "npm run test" para ejecutar los tests y "npm
    run rscript" para ejecutar el script de R (que muestra la lista de
    aristas, matriz de adyacencia, etc. de un grafo).

---
+++ client/src/grafos/grafos.js
@@ -0,0 +1,27 @@
+function adyacencia(aristas, dirigido = false) {
+  const n = mayor(aristas) + 1;
+
+  // Crea una matriz de n x n ceros.
+  var matriz = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+  for (const arista of aristas) {
+    const [i, j] = arista;
+    matriz[i][j] = 1;
+
+    // La matriz de adyacencia de un grafo no dirigido es simétrica.
+    if (dirigido === false) {
+      matriz[j][i] = 1;
+    }
+  }
+
+  return matriz;
+}
+
+// Retorna el número mayor de una matriz.
+function mayor(matriz) {
+  // Crea un arreglo con los números mayores de cada fila en `matriz`.
+  var mayores = matriz.map((fila) => Math.max(...fila));
+  return Math.max(...mayores);
+}
+
+module.exports = { adyacencia, mayor };
